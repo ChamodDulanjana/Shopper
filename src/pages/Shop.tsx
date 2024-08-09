f44299cf0374eac4e5f43cecf8b4c2231ebb0024
ChamodDulanjana <chamodperera128@gmail.com>
2024-08-09 17:58:09 +0530

    Adding new pages

---
+++ src/pages/Shop.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Hero from "../component/hero/Hero.tsx";
+
+const Shop = () => {
+    return (
+        <div>
+            <Hero/>
+        </div>
+    );
+};
+
+export default Shop;
