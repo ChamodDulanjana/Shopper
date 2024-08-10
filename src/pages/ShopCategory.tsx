074be907455af4d687a39a5669cc2cb0719283c7
ChamodDulanjana <chamodperera128@gmail.com>
2024-08-10 13:32:22 +0530

    Render shop category page

--- src/pages/ShopCategory.tsx
+++ src/pages/ShopCategory.tsx
@@ -13,25 +13,37 @@ const ShopCategory = ({banner, category}:ShopCategoryProps) => {
     const all_products = useContext(ShopContext);
     return (
         <div>
-            <img src={banner}  className="w-[1250px]"/>
-            <div>
-                <p>
-                    <span>Showing 1-12</span> out of 36 products
-                </p>
-                <div>
-                    Sort By <img src={dropDown_icon} alt=""/>
+            <img src={banner} className="w-[80%] block m-10 mb-0 mx-auto"/>
+
+            <div className="flex justify-center items-center w-full">
+                <div className="flex my-16 justify-between items-center w-[80%]">
+                    <p>
+                        <span className="font-bold">Showing 1-12</span> out of 36 products
+                    </p>
+                    <div className="px-5 py-3 rounded-[25px] border-[1px] border-[#888] mr-5">
+                        <p>Sort By </p>
+                        {/*<img src={dropDown_icon}/>*/}
+                    </div>
                 </div>
             </div>
 
-            <div>
-                {all_products.map((item, index) => {
-                    if(category === item.category){
-                        return <Item key={index} id={item.id} image={item.image as string} name={item.name} new_price={item.new_price} old_price={item.old_price}/>
-                    }else {
-                        return null;
-                    }
-                })}
+            <div className="flex justify-center items-center w-full">
+                <div className="grid grid-cols-4 mb-20 mx-24  gap-12">
+                    {all_products.map((item, index) => {
+                        if (category === item.category) {
+                            return <Item key={index} id={item.id} image={item.image as string} name={item.name}
+                                         new_price={item.new_price} old_price={item.old_price}/>
+                        } else {
+                            return null;
+                        }
+                    })}
+                </div>
             </div>
+            <div className="flex justify-center items-center m-auto w-[160px] h-[50px] rounded-[75px] bg-[#ededed]
+            text-[#787878] text-sm font-medium cursor-pointer active:font-[10px] mb-16">
+                Explore More
+            </div>
+
         </div>
     );
 };
