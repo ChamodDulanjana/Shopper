d4490b6c370f1f1eb9e5a2e22544797e97308eea
ChamodDulanjana <chamodperera128@gmail.com>
2024-08-14 11:09:39 +0530

    Make NewCollections component responsive

--- src/component/newCollections/NewCollections.tsx
+++ src/component/newCollections/NewCollections.tsx
@@ -4,10 +4,10 @@ import Item from "../item/Item.tsx";
 
 const NewCollections = () => {
     return (
-        <div className="flex flex-col items-center gap-3 h-[90vh] mt-10 mb-96">
-            <h1 className="text-[#171717] text-3xl font-medium mt-10">NEW COLLECTIONS</h1>
+        <div className="flex flex-col items-center gap-3 my-10">
+            <h1 className="text-[#171717] text-3xl font-medium mt-10 max-[800px]:text-2xl">NEW COLLECTIONS</h1>
             <hr className="w-44 h-1 rounded-full bg-[#252525] mb-5"/>
-            <div className="grid grid-cols-4 mt-16 gap-14">
+            <div className="flex flex-wrap m-16 gap-14 items-center justify-center">
                 {new_collections.map((item, index) => {
                     return <Item key={index} id={item.id} image={item.image as string} name={item.name}
                                  new_price={item.new_price} old_price={item.old_price}/>
