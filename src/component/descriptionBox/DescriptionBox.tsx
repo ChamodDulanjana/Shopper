d786c0ca73af1418b6e5cab1eff59a4ea9a898ec
ChamodDulanjana <chamodperera128@gmail.com>
2024-08-14 18:26:32 +0530

    Make Related products component responsive

--- src/component/descriptionBox/DescriptionBox.tsx
+++ src/component/descriptionBox/DescriptionBox.tsx
@@ -1,9 +1,12 @@
-import React, {LegacyRef, useRef, useState} from 'react';
+import React, {LegacyRef, useEffect, useRef, useState} from 'react';
 import {DescriptionBoxObj} from "../../assets/DescriptionBoxList.tsx";
 
 
 const DescriptionBox = () => {
-    const [value, setValue] = useState("");
+    const [value, setValue] = useState("Casual shirt to wear out alert! Check out our new all-cotton flannel shirt! This shirt is made from 100% cotton, making it soft to the touch and gentle on the skin. The long-sleeve design is perfectfor those cooler days or nights, while the button-up closure makes it easy to take on and off. Theplaid design is perfect for any casual occasion, whether going to a barbecue or running errands. The shirt is made in Italy, so you know you’re getting a high-quality product." );
+
+
+    //const defaultValue = "Casual shirt to wear out alert! Check out our new all-cotton flannel shirt! This shirt is made from 100% cotton, making it soft to the touch and gentle on the skin. The long-sleeve design is perfectfor those cooler days or nights, while the button-up closure makes it easy to take on and off. Theplaid design is perfect for any casual occasion, whether going to a barbecue or running errands. The shirt is made in Italy, so you know you’re getting a high-quality product.";
 
     const changeProperty = (type) =>{
         DescriptionBoxObj.map((value)=>{
