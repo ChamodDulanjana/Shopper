fb7ae16b8e1b74d6c9820f3f288a995f4eeaebb4
ChamodDulanjana <chamodperera128@gmail.com>
2024-08-14 17:45:07 +0530

    Make Product Display page responsive

--- src/component/productDisplay/ProductDisplay.tsx
+++ src/component/productDisplay/ProductDisplay.tsx
@@ -6,37 +6,40 @@ import {ShopContext} from "../../context/ShopContext.tsx";
 
 const ProductDisplay = ({props}: ProductProp) => {
     const product = props;
-    const sizesStyles = "bg-[#F0EEF3] border-[#8c8c8c] border-[1px] w-10 h-10 flex items-center justify-center rounded-[3px] cursor-pointer";
+    const sizesStyles = "bg-[#F0EEF3] border-[#8c8c8c] border-[1px] w-10 h-10 flex items-center justify-center rounded-[3px] " +
+        "cursor-pointer max-[552px]:text-sm max-[552px]:w-8 max-[552px]:h-8";
+    const smImgStyles = "w-[110px] max-[850px]:w-[65px]";
+    const starImgStyles = "max-[552px]:h-[13px]";
 
     const {addToCart} = useContext(ShopContext);
 
     return (
-        <div className="flex mx-24 mt-10 mb-20">
-            <div className="flex gap-4">
+        <div className=" mx-20 mt-10 mb-20 flex max-[850px]:flex-col max-[850px]:items-center">
+            <div className="flex gap-4 max-[850px]:mb-20">
                 <div className="flex flex-col gap-4">
-                    <img src={product.image} className="w-[450px]"/>
-                    <img src={product.image} alt=""/>
-                    <img src={product.image} alt=""/>
-                    <img src={product.image} alt=""/>
+                    <img src={product.image} className={smImgStyles}/>
+                    <img src={product.image} className={smImgStyles}/>
+                    <img src={product.image} className={smImgStyles}/>
+                    <img src={product.image} className={smImgStyles}/>
                 </div>
                 <div>
-                    <img src={product.image} className="w-[2000px]"/>
+                    <img src={product.image} className="w-[500px] max-[850px]:w-[300px]"/>
                 </div>
             </div>
 
-            <div className="mx-16 flex flex-col gap-4">
-                <h1 className="text-[#3d3d3d] text-3xl font-medium">{product.name}</h1>
+            <div className="ml-16 mr-5 flex flex-col gap-4 w-[50%] max-[850px]:w-[80%] max-[850px]:ml-0">
+                <h1 className="text-[#3d3d3d] text-3xl font-medium max-xl:text-2xl max-[552px]:text-xl">{product.name}</h1>
                 <div className="flex h-4 gap-1">
-                    <img src={star_icon} alt=""/>
-                    <img src={star_icon} alt=""/>
-                    <img src={star_icon} alt=""/>
-                    <img src={star_icon} alt=""/>
-                    <img src={star_dull_icon} alt=""/>
-                    <p className="text-sm">(134)</p>
+                    <img src={star_icon} className={starImgStyles}/>
+                    <img src={star_icon} className={starImgStyles}/>
+                    <img src={star_icon} className={starImgStyles}/>
+                    <img src={star_icon} className={starImgStyles}/>
+                    <img src={star_dull_icon} className={starImgStyles}/>
+                    <p className="text-sm max-[552px]:text-[12px]">(134)</p>
                 </div>
                 <div className="flex my-5 gap-3 font-medium">
-                    <div className="text-lg">${product.new_price}</div>
-                    <s className="text-sm pt-1 text-[#8c8c8c]">${product.old_price}</s>
+                    <div className="text-lg max-[552px]:text-[16px]">${product.new_price}</div>
+                    <s className="text-sm pt-1 text-[#8c8c8c] max-[552px]:text-[12px]">${product.old_price}</s>
                 </div>
                 <div className="text-sm">
                     Casual shirt to wear out alert! Check out our new all-cotton flannel shirt! This shirt is made from
@@ -46,7 +49,7 @@ const ProductDisplay = ({props}: ProductProp) => {
                     he shirt is made in Italy, so you know you’re getting a high-quality product.
                 </div>
                 <div>
-                    <h1 className="text-lg font-medium">Select Size</h1>
+                    <h1 className="text-lg font-medium max-[552px]:text-[16px]">Select Size</h1>
                     <div className="flex my-5 gap-4">
                         <div className={sizesStyles}>S</div>
                         <div className={sizesStyles}>M</div>
@@ -58,7 +61,7 @@ const ProductDisplay = ({props}: ProductProp) => {
                 <button
                     onClick={() => addToCart(product.id)}
                     className="text-white bg-[#FF4141] mb-10 border-none outline-none cursor-pointer w-48 h-14
-                active:text-[15px] active:transition duration-500" >
+                active:text-[15px] active:transition duration-500 max-[552px]:w-40 max-[552px]:h-12 max-[552px]:text-sm" >
                     ADD TO CART
                 </button>
                 <p className="text-sm"><span className="font-bold">Category : </span>Women, T-Shirt, Crop Top</p>
